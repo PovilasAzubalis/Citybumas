c8354e853e11067adde94b95fa1b170e46a3774d
PovilasAzubalis <azubalispovilas@gmail.com>
2025-09-06 17:12:19 +0300

    Delete Online-Music-Store-master directory

--- Online-Music-Store-master/js/homepage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("nav-link").click(function() {
-        $('html,body').animate({
-            scrollTop: $(".main-content").offset().top},
-            'slow');
-    });
-});
